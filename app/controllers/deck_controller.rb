c1678e54f171453bd0fbccde9fa83c3a2e64f729
Beauregard <beauregard@Beauregards-iMac.local>
2017-08-01 20:19:44 -0500

    cleaned up the code

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -2,45 +2,31 @@ require 'mtg_sdk'
 
 class DeckController < ApplicationController
   def index
-    @name = "Kird Apes"
-    @description = "monkey to the face"
+    @name = "Ageless Goblet"
+    @description = "Ageless Entity to the Face"
+
     # grab the deck from the disk
-    contents = IO.readlines("/tmp/kird-apes.deck")
+    contents = IO.readlines("AgelessGoblet.deck")
     location = 'main'
-    @deck = {
-      'main' => {},
-      'sideboard' => {}
-    }
-    @stats = {
-      'types' => {}
-    }
+
+    @deck = {'main' => {}, 'sideboard' => {} }
+
+    @stats = {'types' => {} }
+
     # go over every line of the deck file
     contents.each do |line|
-      # remove the whitespace from the ends of the lines
-      line.strip!
+      line.strip!  # remove the whitespace from the ends of the lines
       if line == ""
         location = 'sideboard'
         next
       end
+
       parts = line.split(' ')
       card_count = parts.shift()
       card_name = parts.join(' ')
-
       card_data = MTG::Card.where(name: card_name).all.first
-      (card_data.supertypes || []).each do |type|
-        if @stats['types'].key? card_data.type
-          @stats['types'][card_data.type] += 1
-        else
-          @stats['types'][card_data.type] = 1
-        end
-      end
-      @deck[location][card_name] = {
-        'count' => card_count,
-        'data' => card_data
-      }
 
-
+      @deck[location][card_name] = { 'count' => card_count, 'data' => card_data }
     end
-
   end
 end
