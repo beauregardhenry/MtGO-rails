df9075d55adf62fd9ba459195d9186ddcea74e1a
beauregardhenry <beauregard.henry@gmail.com>
2017-08-21 20:06:11 -0500

    Fixed A Sideboard Bug

--- app/controllers/deck_controller.rb
+++ app/controllers/deck_controller.rb
@@ -2,7 +2,7 @@ require 'mtg_sdk'
 
 class DeckController < ApplicationController
   def index
-    targetDeckID = "9"
+    targetDeckID = "8"
     selected_deck = Deck.deck_selector(targetDeckID)
     sorted_deck = Deck.deck_sorter(selected_deck)
     augmented_deck = Deck.write_deck_to_database(sorted_deck, selected_deck)
