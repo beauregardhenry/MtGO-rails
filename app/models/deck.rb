6c607cf089cd64dd5d22ff993aa68b566aa66e69
Jake Gaylor <jgaylor@fsw.com>
2017-08-24 17:36:19 -0600

    add some protection around cards that aren't found

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -55,6 +55,11 @@ class Deck < ApplicationRecord
     sorted_deck.each do |deck_location, subdeck|
       subdeck.each do |card_name, card_count|
         card = Card.where("lower(name) = ?", card_name.downcase).where.not(set_name: 'vanguard', image_url: nil).last
+        if card.nil?
+          puts "#{card_name} was not found in the database."
+          # TODO: we should really tell the user about this error and not just skip the card
+          next
+        end
         unless deck.cards.include?(card)
           deck.cards << card # this is where the magic happens, once you have a deck and a card
         end # end unless deck.cards.include?
